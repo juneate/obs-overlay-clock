0394de1ef0f746992119e253b6e3cef3d537554c
Rocco P <roccopan@gmail.com>
2021-09-15 12:08:37 -0400

    fixed 12pm

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ setInterval(()=>{
   const d = new Date()
   // const s = d.getSeconds().toString().padStart(2, '0')
   const m = d.getMinutes().toString().padStart(2, '0')
-  const h = (d.getHours() % 12).toString().padStart(2, '0')
-  clock.innerHTML = `${h}<span class="blink">:</span>${m}<small>${d.getHours() > 12 ? `pm` : `am`}</small>`
+  const h = (d.getHours() > 12 ? d.getHours() - 12 : d.getHours()).toString().padStart(2, '0')
+  clock.innerHTML = `${h}<span class="blink">:</span>${m}<small>${d.getHours() >= 12 ? `pm` : `am`}</small>`
     //("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2)
 }, 1000)
